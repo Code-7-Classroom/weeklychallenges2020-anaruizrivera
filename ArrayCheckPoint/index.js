2b36ead3a43c9a3bcbc1145e7d321088ebb3b330
anaruizrivera <anaruizrivera91@icloud.com>
2020-03-30 11:12:02 -0400

    adding checkpoint

---
+++ ArrayCheckPoint/index.js
@@ -0,0 +1,8 @@
+//Tree JS Array methods
+//Boolean, Number, 
+
+var drinks =[ 'water', 'coffee', 'tea'];
+var foods=['apple', 'sandwich', 'chips'];
+var combine = [drinks,foods];
+combine =[foods, drinks];
+console.log(combine);
