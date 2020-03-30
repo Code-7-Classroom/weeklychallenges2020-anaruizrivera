8d106f43fa4edbc84801cd98a195981731b03e1f
anaruizrivera <anaruizrivera91@icloud.com>
2020-03-30 13:46:51 -0400

    first push

---
+++ Js02Week/Easy Challenge/easy.js
@@ -0,0 +1,5 @@
+var firstStudent=['Ruiz', 'Ana', 18];
+var secondStudent=['Bueso', 'Darwin', 18];
+var thirdStudent=['Garcia', 'Fatima', 17];
+
+console.log('Hello, my name is ' + secondStudent[1]+ ' ' + secondStudent[0] + 'and Im' + secondStudent[2]+ 'years old');
