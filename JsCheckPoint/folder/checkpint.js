7d6aa335fc780edd32e033bb03fbf6a0c30796be
anaruizrivera <anaruizrivera91@icloud.com>
2020-04-13 16:58:29 -0400

    Js Checkpoint

---
+++ JsCheckPoint/folder/checkpint.js
@@ -0,0 +1,15 @@
+//How is information store in an Object
+//
+//1
+let me= {
+    name="Ana",
+    lastName="Ruiz"
+}
+console.log(me);
+
+
+//2
+const sayHell6= (name) => {
+    console.log("Hello "+ name);
+}
+sayHello("Ana");
