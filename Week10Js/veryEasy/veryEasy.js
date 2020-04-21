ff9c9fccc9280d36eb38d2d5a9adda356827217c
anaruizrivera <anaruizrivera91@icloud.com>
2020-04-20 21:42:52 -0400

    updating

--- Week10Js/veryEasy/veryEasy.js
+++ Week10Js/veryEasy/veryEasy.js
@@ -2,11 +2,13 @@
 var, let and const.  Please provide a written explanation. You are able to 
 include a code reference along with your written explanation for each keyword.*/
 
-//var -Global scope, it can be call anywhere in the code
+//var -Global scope, it can be call anywhere in the code.
 
-//let- is not a global scope, it can only be called/ invoke in the same block scope
+//let- is not a global scope, it can only be called/ invoke in the same block scope, contain the element in the body that is defined(inside the block scope).
 
-//const - cannot be reasiggned
+//const - cannot be reasiggned, is only define one time,
+// it cannot be change.
+
 
 
 
